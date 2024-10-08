dc5ec3b35d1110080fd9f8092cbc3c22f9a9b21d
circled-square <46634383+circled-square@users.noreply.github.com>
2024-10-08 15:52:57 +0200

    feature: asserts now throw custom exception

--- include/slogga/asserts.hpp
+++ include/slogga/asserts.hpp
@@ -17,20 +17,29 @@
     // Actual implementation for debug builds
     #include <source_location>
     #include "./log.hpp"
-    #define EXPECTS(x) (slogga::detail::assertion(x, #x, "precondition"))
-    #define ENSURES(x) (slogga::detail::assertion(x, #x, "postcondition"))
-    #define ASSERTS(x) (slogga::detail::assertion(x, #x, "assertion"))
+    #define EXPECTS(x) (slogga::assertion(x, #x, "precondition"))
+    #define ENSURES(x) (slogga::assertion(x, #x, "postcondition"))
+    #define ASSERTS(x) (slogga::assertion(x, #x, "assertion"))
 
-    namespace slogga::detail {
+    namespace slogga {
+        class assertion_failed : std::exception {
+            const char* m_cond_str;
+            const char* m_assertion_type;
+            std::source_location m_sl;
+            mutable std::string m_what_string_cache = std::string();
+        public:
+            const char* what() const {
+                if(m_what_string_cache.empty()) {
+                    m_what_string_cache =  std::format("{} check failed at function '{}' at {}:{}:{}; '{}' evaluated to false",
+                        m_assertion_type, m_sl.function_name(), m_sl.file_name(), m_sl.line(), m_sl.column(), m_cond_str);
+                }
+                return m_what_string_cache.c_str();
+            }
+        };
 
         inline void assertion(bool cond, const char* cond_str, const char* assertion_type, std::source_location sl = std::source_location::current()) {
             if(!cond) {
-
-                std::string err = std::format("{} check failed at function '{}' at {}:{}:{}; '{}' evaluated to false",
-                    assertion_type, sl.function_name(), sl.file_name(), sl.line(), sl.column(), cond_str);
-
-                slogga::stdout_log.fatal(err);
-                throw std::runtime_error(err);
+                throw assertion_failed(cond_str, assertion_type, std::move(sl));
             }
         }
     }
