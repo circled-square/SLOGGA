fc603d7329856f057f557202b98a6283a6532fd4
circled-square <46634383+circled-square@users.noreply.github.com>
2025-02-12 19:44:54 +0100

    feat: added UNIMPLEMENTED assertion

--- include/slogga/asserts.hpp
+++ include/slogga/asserts.hpp
@@ -23,41 +23,49 @@
 #ifdef NDEBUG
     // Empty implementation for release builds
 
-    #define EXPECTS(expr) SLOGGA_ASSUME(expr)
-    #define ENSURES(expr) SLOGGA_ASSUME(expr)
-    #define ASSERTS(expr) SLOGGA_ASSUME(expr)
+    #define EXPECTS(x) SLOGGA_ASSUME(x)
+    #define ENSURES(x) SLOGGA_ASSUME(x)
+    #define ASSERTS(x) SLOGGA_ASSUME(x)
+
+    // UNIMPLEMENTED is not empty even for release builds: the input that causes it to fail is 
+    // actually valid and the user does not deserve to get undefined behaviour (that's what 
+    // assume does) rather than a crash/exception: instead they'd rather always pay the small 
+    // price of an assertion. Besides, ideally, any UNIMPLEMENTED check is eventually removed, 
+    // whereas other assertions are meant to stay indefinitely.
+    #define UNIMPLEMENTED(x) (slogga::assertion(x, #x, "this feature is unimplemented; assertion"))
 #else
     // Actual implementation for debug builds
     #define EXPECTS(x) (slogga::assertion(x, #x, "precondition check"))
     #define ENSURES(x) (slogga::assertion(x, #x, "postcondition check"))
     #define ASSERTS(x) (slogga::assertion(x, #x, "assertion"))
-
-    namespace slogga {
-        class assertion_failed_exception : public std::exception {
-            const char* m_cond_str;
-            const char* m_assertion_type;
-            std::source_location m_sl;
-            mutable std::string m_what_string_cache = std::string();
-        public:
-            assertion_failed_exception(const char* cond_str, const char* assertion_type, std::source_location sl) :
-                m_cond_str(cond_str), m_assertion_type(assertion_type), m_sl(std::move(sl)) {}
-
-            virtual const char* what() const noexcept override {
-                if(m_what_string_cache.empty()) {
-                    m_what_string_cache =  std::format("{} failed at function '{}' at {}:{}:{}; '{}' evaluated to false",
-                        m_assertion_type, m_sl.function_name(), m_sl.file_name(), m_sl.line(), m_sl.column(), m_cond_str);
-                }
-                return m_what_string_cache.c_str();
-            }
-        };
-
-        inline void assertion(bool cond, const char* cond_str, const char* assertion_type, std::source_location sl = std::source_location::current()) {
-            if(!cond) {
-                throw assertion_failed_exception(cond_str, assertion_type, std::move(sl));
-            }
-        }
-    }
+    #define UNIMPLEMENTED(x) (slogga::assertion(x, #x, "this feature is unimplemented; assertion"))
 #endif
 
+namespace slogga {
+    class assertion_failed_exception : public std::exception {
+        const char* m_cond_str;
+        const char* m_assertion_type;
+        std::source_location m_sl;
+        mutable std::string m_what_string_cache = std::string();
+    public:
+        assertion_failed_exception(const char* cond_str, const char* assertion_type, std::source_location sl) :
+            m_cond_str(cond_str), m_assertion_type(assertion_type), m_sl(std::move(sl)) {}
+
+        virtual const char* what() const noexcept override {
+            if(m_what_string_cache.empty()) {
+                m_what_string_cache =  std::format("{} failed at function '{}' at {}:{}:{}: '{}' evaluated to false",
+                    m_assertion_type, m_sl.function_name(), m_sl.file_name(), m_sl.line(), m_sl.column(), m_cond_str);
+            }
+            return m_what_string_cache.c_str();
+        }
+    };
+
+    namespace detail {
+    inline void assertion(bool cond, const char* cond_str, const char* assertion_type, std::source_location sl = std::source_location::current()) {
+        if(!cond) {
+            throw assertion_failed_exception(cond_str, assertion_type, std::move(sl));
+        }
+    }
+}
 
 #endif // SLOGGA_ASSERTS_HPP
